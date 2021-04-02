c22b6b61d704c131b6495d0d5a09b1f7f0c4dc01
Ashiemotto <ashheadley@gmail.com>
2021-04-01 21:02:24 -0400

    add clear button

--- script.js
+++ script.js
@@ -176,5 +176,9 @@ var x = [9,10,11,12,1,2,3,4,5]
         localStorage.setItem(time, inputText);
     });
     
-    
+    $('.clear').click( function() {
+
+        localStorage.clear();
+       
+       });
     
